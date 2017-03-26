44a57468d55fcd81bd5704a02ad25225dc69f052
Christophe De Busschop <debusschopchristophe@gmail.com>
2017-03-26 09:32:30 +0200

    added background images to resources, added method for changing background, changed form to fullscreen, added method for checking if window has focus but still unimplemented

--- Examenapplicatie/Examenapplicatie/Form1.Designer.cs
+++ Examenapplicatie/Examenapplicatie/Form1.Designer.cs
@@ -28,10 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(windowMain));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.achtergrondHerstellenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.afsluitenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testknop1 = new System.Windows.Forms.Button();
+            this.testknop2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,6 +62,7 @@
             this.achtergrondHerstellenToolStripMenuItem.Name = "achtergrondHerstellenToolStripMenuItem";
             this.achtergrondHerstellenToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
             this.achtergrondHerstellenToolStripMenuItem.Text = "Achtergrond herstellen";
+            this.achtergrondHerstellenToolStripMenuItem.Click += new System.EventHandler(this.achtergrondHerstellenToolStripMenuItem_Click);
             // 
             // afsluitenToolStripMenuItem
             // 
@@ -66,15 +70,40 @@
             this.afsluitenToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
             this.afsluitenToolStripMenuItem.Text = "Afsluiten";
             // 
+            // testknop1
+            // 
+            this.testknop1.Location = new System.Drawing.Point(347, 146);
+            this.testknop1.Name = "testknop1";
+            this.testknop1.Size = new System.Drawing.Size(75, 23);
+            this.testknop1.TabIndex = 1;
+            this.testknop1.Text = "OK";
+            this.testknop1.UseVisualStyleBackColor = true;
+            this.testknop1.Click += new System.EventHandler(this.testknop1_Click);
+            // 
+            // testknop2
+            // 
+            this.testknop2.Location = new System.Drawing.Point(462, 146);
+            this.testknop2.Name = "testknop2";
+            this.testknop2.Size = new System.Drawing.Size(75, 23);
+            this.testknop2.TabIndex = 2;
+            this.testknop2.Text = "NOK";
+            this.testknop2.UseVisualStyleBackColor = true;
+            this.testknop2.Click += new System.EventHandler(this.testknop2_Click);
+            // 
             // windowMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(926, 427);
+            this.Controls.Add(this.testknop2);
+            this.Controls.Add(this.testknop1);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "windowMain";
             this.Text = "Examen";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -88,6 +117,8 @@
         private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem achtergrondHerstellenToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem afsluitenToolStripMenuItem;
+        private System.Windows.Forms.Button testknop1;
+        private System.Windows.Forms.Button testknop2;
     }
 }
 
--- Examenapplicatie/Examenapplicatie/Form1.cs
+++ Examenapplicatie/Examenapplicatie/Form1.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Examenapplicatie.Properties;
 
 namespace Examenapplicatie
 {
@@ -15,6 +19,77 @@ namespace Examenapplicatie
         public windowMain()
         {
             InitializeComponent();
+        }
+
+
+
+        private void achtergrondHerstellenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //
+        //  Background methods
+        //
+
+        private void changeBackground(bool OK)
+        {
+            if (OK)
+            {
+                try
+                {
+                    ActiveForm.BackgroundImage = Resources.backgroundOK;
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    MessageBox.Show("Backgroundimage not found");
+                }
+            }
+            else
+            {
+                try
+                {
+                    ActiveForm.BackgroundImage = Resources.backgroundNOK;
+                }
+                catch (System.IO.FileNotFoundException)
+                {
+                    MessageBox.Show("Backgroundimage not found");
+                }
+                
+            }
+        }
+
+        // http://stackoverflow.com/questions/7162834/determine-if-current-application-is-activated-has-focus
+        /// <summary>Returns true if the current application has focus, false otherwise</summary>
+        private static bool ApplicationIsActivated()
+        {
+            var activatedHandle = GetForegroundWindow();
+            if (activatedHandle == IntPtr.Zero)
+            {
+                return false;       // No window is currently activated
+            }
+
+            var procId = Process.GetCurrentProcess().Id;
+            int activeProcId;
+            GetWindowThreadProcessId(activatedHandle, out activeProcId);
+
+            return activeProcId == procId;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
+        private void testknop1_Click(object sender, EventArgs e)
+        {
+            changeBackground(true);
+        }
+
+        private void testknop2_Click(object sender, EventArgs e)
+        {
+            changeBackground(false);
         }
     }
 }
